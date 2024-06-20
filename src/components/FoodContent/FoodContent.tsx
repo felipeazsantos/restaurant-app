8a7f3167a699f01efcca04cfc35c364fbc351853
Felipe Azevedo <felipe.azevedo@cws.digital>
2024-06-20 20:58:25 -0300

    adding view allergy information link

--- src/components/FoodContent/FoodContent.tsx
+++ src/components/FoodContent/FoodContent.tsx
@@ -3,12 +3,14 @@ import { FoodItemsContainer } from './FoodItemsContainer/FoodItemsContainer';
 import { IFoodContent } from './interfaces/IFoodContent';
 import { FoodModalBasket } from '../FoodCheckout/FoodModalBasket/FoodModalBasket';
 import { ButtonCheckout } from '../FoodCheckout/ButtonCheckout/ButtonCheckout';
-import { Box } from '@mui/material';
+import { Box, Link } from '@mui/material';
 import { useOrders } from '../../hooks/useOrders';
+import { useWebSettings } from '../../hooks/useWebSettings';
 
 export const FoodContent: FC<IFoodContent> = (props): ReactElement => {
     const [modalOpen, setModalOpen] = useState<boolean>(false);
     const orders = useOrders();
+    const { primaryColour } = useWebSettings();
 
     const handleModalClose = () => setModalOpen(false);
     const handleButtonCheckoutClick = () => setModalOpen(true);
@@ -17,6 +19,17 @@ export const FoodContent: FC<IFoodContent> = (props): ReactElement => {
         <>
             <FoodItemsContainer {...props} />
             <Box bgcolor="#F8F9FA" paddingTop="24px">
+                <Box
+                    borderRadius="8px"
+                    mb="30px"
+                    mx="16px"
+                    display="flex"
+                    justifyContent="center"
+                    alignItems="center"
+                    bgcolor="#ffffff"
+                >
+                    <Link color={primaryColour} fontWeight="bold">View allergy information</Link>
+                </Box>
                 {orders.length > 0 && <ButtonCheckout counter={orders.length} handleClick={handleButtonCheckoutClick} buttonText="Your basket" />}
             </Box>
             <FoodModalBasket open={modalOpen} onClose={handleModalClose} orders={orders} />
