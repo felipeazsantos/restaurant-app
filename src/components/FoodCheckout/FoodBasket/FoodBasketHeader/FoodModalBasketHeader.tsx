389c77175407ba7b068b8691ef07611b533a709c
Felipe Azevedo <felipe.azevedo@cws.digital>
2024-06-21 19:37:55 -0300

    FoodBasket component header adjustment

--- src/components/FoodCheckout/FoodBasket/FoodBasketHeader/FoodModalBasketHeader.tsx
+++ src/components/FoodCheckout/FoodBasket/FoodBasketHeader/FoodModalBasketHeader.tsx
@@ -2,31 +2,37 @@ import { Close } from '@mui/icons-material';
 import { Box, IconButton, Typography } from '@mui/material';
 import React, { FC, ReactElement } from 'react';
 import { IFoodBasketHeader } from '../../interfaces/IFoodBasketHeader';
+import { useIsMobScreen } from '../../../../hooks/useIsMobScreen';
 
 export const FoodBasketHeader: FC<IFoodBasketHeader> = (props): ReactElement => {
     const { onClose } = props;
+    const isMobScreen = useIsMobScreen();
+
     return (
         <Box
             display="flex"
-            justifyContent="center"
-            bgcolor="#ffffff"
+            justifyContent={isMobScreen ? "center" : "start"}
+            bgcolor={isMobScreen ? "#ffffff" : "#F8F9FA"}
             alignItems="center"
             position="relative"
             width="100%"
-            py="24px"
+            py={isMobScreen ? "24px" : "16px"}
+            ml={isMobScreen ? "inherited" : "24px"}
         >
             <Typography color="#121212" variant="h1" fontSize="18px" fontWeight="500">
                 Basket
             </Typography>
-            <IconButton
-                onClick={onClose}
-                size="large"
-                edge="start"
-                aria-label="basket"
-                sx={{ mr: 2, position: 'absolute', right: '-16px' }}
-            >
-                <Close />
-            </IconButton>
+            {isMobScreen &&
+                <IconButton
+                    onClick={onClose}
+                    size="large"
+                    edge="start"
+                    aria-label="basket"
+                    sx={{ mr: 2, position: 'absolute', right: '-16px' }}
+                >
+                    <Close />
+                </IconButton>
+            }
         </Box>
     )
 }
